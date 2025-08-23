5be88cfcc779f9b3e5ef035efa3dc16d4d5904c0
arkhiVd <aravindakrishnannv@tutamail.com>
2025-08-23 22:16:53 +0000

    feat: Add complete frontend infrastructure and website files

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "bucketname" {
+  default ="portfolio-bucket-3258765923"
+}
+
+variable "domainname" {
+  default ="aravindakrishnan.click"
+}
+
+variable "subject_alternative_names" {
+  default =["aravindakrishnan.click","www.aravindakrishnan.click"]
+  
+}
+

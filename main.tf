56b6540538d837ff365f4f0b7410481729ca43c3
arkhiVd <95763443+arkhiVd@users.noreply.github.com>
2025-08-28 12:36:24 +0000

    Region Clean Up

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -7,29 +6,34 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = var.aws_region
 }
+
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
 }
+
 terraform {
   backend "s3" {
     bucket = "aravind-terraform-state-bucket-ap-south-1"
     key    = "portfolio-frontend/terraform.tfstate"
-    region = "ap-south-1"                                
+    region = "ap-south-1"
   }
 }
-# file extensions to MIME types
+
 locals {
   mime_types = {
-    "html" = "text/html"
-    "css"  = "text/css"
-    "js"   = "application/javascript"
-    "jpeg" = "image/jpeg"
-    "jpg"  = "image/jpeg"
-    "png"  = "image/png"
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "jpeg" = "image/jpeg",
+    "jpg"  = "image/jpeg",
+    "png"  = "image/png",
+    "ico"  = "image/vnd.microsoft.icon",
+    "txt"  = "text/plain"
   }
 }
 
@@ -37,10 +41,8 @@ locals {
 # tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "portfolio_bucket" {
   bucket = var.bucketname
-  timeouts {
-    create = "5m"
-  }
 }
+
 # tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "portfolio_bucket_encryption" {
   bucket = aws_s3_bucket.portfolio_bucket.id
@@ -68,44 +70,19 @@ resource "aws_s3_bucket_public_access_block" "portfolio_public_access_block" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "portfolio_bucket_policy" {
-  bucket = aws_s3_bucket.portfolio_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontOAC"
-        Effect    = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.portfolio_bucket.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.portfolio_cdn.arn
-          }
-        }
-      },
-    ]
-  })
-}
-
-# revisit this
 resource "aws_s3_object" "portfolio_files" {
-  for_each = fileset("my-website-files/", "**")
+  for_each = fileset("${path.module}/my-website-files/", "**")
 
   bucket       = aws_s3_bucket.portfolio_bucket.id
   key          = each.value
-  source       = "my-website-files/${each.value}"
+  source       = "${path.module}/my-website-files/${each.value}"
   content_type = lookup(local.mime_types, regex("\\.(\\w+)$", each.value)[0], "binary/octet-stream")
-  etag         = filemd5("my-website-files/${each.value}")
+  etag         = filemd5("${path.module}/my-website-files/${each.value}")
 }
 
 resource "aws_cloudfront_origin_access_control" "portfolio_oac" {
-  name                              = "OAC for ${var.bucketname}"
-  description                       = "Origin Access Control for S3 bucket"
+  name                              = "OAC-${var.bucketname}"
+  description                       = "OAC for S3 bucket"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
@@ -124,23 +101,14 @@ resource "aws_cloudfront_distribution" "portfolio_cdn" {
   default_root_object = "index.html"
 
   aliases = [var.domainname, "www.${var.domainname}"]
- 
+
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${var.bucketname}"
-
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${var.bucketname}"
     viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6" # CachingOptimized
   }
-
-  price_class = "PriceClass_100"
 
   restrictions {
     geo_restriction {
@@ -149,22 +117,37 @@ resource "aws_cloudfront_distribution" "portfolio_cdn" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
-  }
-
-  tags = {
-    Project   = "Cloud Resume Challenge"
-    ManagedBy = "Terraform"
   }
 }
 
+resource "aws_s3_bucket_policy" "portfolio_bucket_policy" {
+  bucket = aws_s3_bucket.portfolio_bucket.id
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = { Service = "cloudfront.amazonaws.com" },
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.portfolio_bucket.arn}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.portfolio_cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_acm_certificate" "portfolio_cert" {
-  provider = aws.us-east-1
-  domain_name       = var.domainname
+  provider                  = aws.us_east_1
+  domain_name               = var.domainname
   subject_alternative_names = ["www.${var.domainname}"]
-  validation_method = "DNS"
+  validation_method         = "DNS"
 }
 
 data "aws_route53_zone" "portfolio_zone" {
@@ -173,7 +156,7 @@ data "aws_route53_zone" "portfolio_zone" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  provider = aws.us-east-1 
+  provider = aws.us_east_1
   for_each = {
     for dvo in aws_acm_certificate.portfolio_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -182,15 +165,16 @@ resource "aws_route53_record" "cert_validation" {
     }
   }
 
-  name    = each.value.name
-  records = [each.value.record]
-  ttl     = 60
-  type    = each.value.type
-  zone_id = data.aws_route53_zone.portfolio_zone.zone_id
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.portfolio_zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  provider = aws.us-east-1
+  provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.portfolio_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
@@ -217,13 +201,4 @@ resource "aws_route53_record" "root_alias" {
     zone_id                = aws_cloudfront_distribution.portfolio_cdn.hosted_zone_id
     evaluate_target_health = false
   }
-}
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket for the website."
-  value       = aws_s3_bucket.portfolio_bucket.bucket # <-- Use the correct local name
-}
-
-output "cloudfront_distribution_id" {
-  description = "The ID of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.portfolio_cdn.id # <-- Use the correct local name
 }
